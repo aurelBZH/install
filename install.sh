4943c87258ee4abcc531081b596db15ff972d20f
aurelien beliard <aurel.beliard@gmail.com>
2020-04-16 07:17:31 +0200

    add install

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y libhdf5-dev
+sudo apt install -y nano
 wget http://cf.10xgenomics.com/samples/cell-vdj/3.0.2/vdj_v1_hs_aggregated_donor1/vdj_v1_hs_aggregated_donor1_binarized_matrix.csv
 wget http://cf.10xgenomics.com/samples/cell-vdj/3.0.2/vdj_v1_hs_aggregated_donor1/vdj_v1_hs_aggregated_donor1_filtered_feature_bc_matrix.h5
 wget http://cf.10xgenomics.com/samples/cell-vdj/3.0.2/vdj_v1_hs_aggregated_donor1/vdj_v1_hs_aggregated_donor1_filtered_feature_bc_matrix.tar.gz
@@ -13,7 +14,7 @@ wget http://cf.10xgenomics.com/samples/cell-vdj/3.0.2/vdj_v1_hs_aggregated_donor
 wget http://cf.10xgenomics.com/samples/cell-vdj/3.0.2/vdj_v1_hs_aggregated_donor1/vdj_v1_hs_aggregated_donor1_consensus_annotations.json
 wget http://cf.10xgenomics.com/samples/cell-vdj/3.0.2/vdj_v1_hs_aggregated_donor1/vdj_v1_hs_aggregated_donor1_vdj_web_summary.html
 wget http://cf.10xgenomics.com/samples/cell-vdj/3.0.2/vdj_v1_hs_aggregated_donor1/vdj_v1_hs_aggregated_donor1_vloupe.vloupe
-
+git config --global core.editor "nano"
 git config --global user.name "aurelien beliard"
 git config --global user.email "aurelien.beliard@laposte.net" 
 Rscript install.R
